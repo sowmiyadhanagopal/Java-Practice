0da22aa52ffecd4d4ce4b4f38780adb393624648
Your Name <sowmi@sowmya>
2025-12-16 09:38:38 +0530

    Added StringDemo in basics

---
+++ src/string/StringDemo.java
@@ -0,0 +1,11 @@
+package string;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String text = "Java Basics";
+
+        System.out.println("Length: " + text.length());
+        System.out.println("Uppercase: " + text.toUpperCase());
+        System.out.println("Contains 'Java': " + text.contains("Java"));
+    }
+}
